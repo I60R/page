ab7a3e7363cf6130fc359557b2f1c113af57be29
160R <160R@protonmail.com>
2018-04-16 17:10:42 +0200

    move Opt into cli module

---
+++ src/cli.rs
@@ -0,0 +1,83 @@
+use structopt::{clap::{ ArgGroup, AppSettings::* }};
+
+
+#[derive(StructOpt)]
+#[structopt(raw(
+    global_settings="&[DisableHelpSubcommand, DeriveDisplayOrder]",
+    group=r#"ArgGroup::with_name("splits")
+        .args(&["split_left", "split_right", "split_above", "split_below"])
+        .args(&["split_left_cols", "split_right_cols", "split_above_rows", "split_below_rows"])
+        .multiple(false)"#,
+    group=r#"ArgGroup::with_name("instances")
+        .args(&["instance", "instance_append"])
+        .multiple(false)"#))]
+pub struct Opt {
+    /// Neovim session address
+    #[structopt(short="s", env="NVIM_LISTEN_ADDRESS")]
+    pub address: Option<String>,
+
+    /// Run command in pager buffer
+    #[structopt(short="e")]
+    pub command: Option<String>,
+
+    /// Use named instance buffer instead of opening new
+    #[structopt(short="i")]
+    pub instance: Option<String>,
+
+    /// The same as "-i" but with append mode
+    #[structopt(short="a")]
+    pub instance_append: Option<String>,
+
+    /// Close named instance buffer
+    #[structopt(short="x")]
+    pub instance_close: Option<String>,
+
+    /// Filetype hint, allows color highlighting when reading from stdin
+    #[structopt(short="t", default_value="pager")]
+    pub filetype: String,
+
+    /// Stay focused on current buffer
+    #[structopt(short="b")]
+    pub back: bool,
+
+    /// Print path to /dev/pty/* associated with pager buffer
+    #[structopt(short="p")]
+    pub print_pty_path: bool,
+
+    /// Split right with ratio: window_width  * 3 / (<r provided> + 1)
+    #[structopt(short="r", parse(from_occurrences))]
+    pub split_right: u8,
+
+    /// Split left  with ratio: window_width  * 3 / (<l provided> + 1)
+    #[structopt(short="l", parse(from_occurrences))]
+    pub split_left: u8,
+
+    /// Split above with ratio: window_height * 3 / (<u provided> + 1)
+    #[structopt(short="u", parse(from_occurrences))]
+    pub split_above: u8,
+
+    /// Split below with ratio: window_height * 3 / (<d provided> + 1)
+    #[structopt(short="d", parse(from_occurrences))]
+    pub split_below: u8,
+
+    /// Split right and resize to <split_right_cols> columns
+    #[structopt(short="R")]
+    pub split_right_cols: Option<u8>,
+
+    /// Split left  and resize to <split_left_cols>  columns
+    #[structopt(short="L")]
+    pub split_left_cols: Option<u8>,
+
+    /// Split above and resize to <split_above_rows> rows
+    #[structopt(short="U")]
+    pub split_above_rows: Option<u8>,
+
+    /// Split below and resize to <split_below_rows> rows
+    #[structopt(short="D")]
+    pub split_below_rows: Option<u8>,
+
+    /// Open these files in separate buffers
+    #[structopt(name="FILES")]
+    pub files: Vec<String>
+}
+
--- src/page.rs
+++ src/page.rs
@@ -8,9 +8,12 @@ extern crate structopt;
 extern crate neovim_lib;
 extern crate rand;
 
+
+mod util;
+mod cli;
+
 use neovim_lib::{self as nvim, neovim_api as nvim_api, NeovimApi, Value};
 use rand::{Rng, thread_rng};
-use structopt::{StructOpt, clap::{ ArgGroup, AppSettings::* }};
 use std::fs::{self, remove_file, File, OpenOptions};
 use std::path::PathBuf;
 use std::io::{self, Read, Write};
@@ -21,90 +24,9 @@ use std::time::Duration;
 use std::net::{SocketAddr};
 use std::error::Error;
 use std::os::unix::fs::FileTypeExt;
+use structopt::StructOpt;
 
-mod util;
-
-
-#[derive(StructOpt)]
-#[structopt(raw(
-    global_settings="&[DisableHelpSubcommand, DeriveDisplayOrder]",
-    group=r#"ArgGroup::with_name("splits")
-        .args(&["split_left", "split_right", "split_above", "split_below"])
-        .args(&["split_left_cols", "split_right_cols", "split_above_rows", "split_below_rows"])
-        .multiple(false)"#,
-    group=r#"ArgGroup::with_name("instances")
-        .args(&["instance", "instance_append"])
-        .multiple(false)"#))]
-struct Opt {
-    /// Neovim session address
-    #[structopt(short="s", env="NVIM_LISTEN_ADDRESS")]
-    address: Option<String>,
-
-    /// Run command in pager buffer
-    #[structopt(short="e")]
-    command: Option<String>,
-
-    /// Use named instance buffer instead of opening new
-    #[structopt(short="i")]
-    instance: Option<String>,
-
-    /// The same as "-i" but with append mode
-    #[structopt(short="a")]
-    instance_append: Option<String>,
-
-    /// Close named instance buffer
-    #[structopt(short="x")]
-    instance_close: Option<String>,
-
-    /// Filetype hint, allows color highlighting when reading from stdin
-    #[structopt(short="t", default_value="pager")]
-    filetype: String,
-
-    /// Stay focused on current buffer
-    #[structopt(short="b")]
-    back: bool,
-
-    /// Print path to /dev/pty/* associated with pager buffer
-    #[structopt(short="p")]
-    print_pty_path: bool,
-
-    /// Split right with ratio: window_width  * 3 / (<r provided> + 1)
-    #[structopt(short="r", parse(from_occurrences))]
-    split_right: u8,
-
-    /// Split left  with ratio: window_width  * 3 / (<l provided> + 1)
-    #[structopt(short="l", parse(from_occurrences))]
-    split_left: u8,
-
-    /// Split above with ratio: window_height * 3 / (<u provided> + 1)
-    #[structopt(short="u", parse(from_occurrences))]
-    split_above: u8,
-
-    /// Split below with ratio: window_height * 3 / (<d provided> + 1)
-    #[structopt(short="d", parse(from_occurrences))]
-    split_below: u8,
-
-    /// Split right and resize to <split_right_cols> columns
-    #[structopt(short="R")]
-    split_right_cols: Option<u8>,
-
-    /// Split left  and resize to <split_left_cols>  columns
-    #[structopt(short="L")]
-    split_left_cols: Option<u8>,
-
-    /// Split above and resize to <split_above_rows> rows
-    #[structopt(short="U")]
-    split_above_rows: Option<u8>,
-
-    /// Split below and resize to <split_below_rows> rows
-    #[structopt(short="D")]
-    split_below_rows: Option<u8>,
-
-    /// Open these files in separate buffers
-    #[structopt(name="FILES")]
-    files: Vec<String>
-}
-
+use cli::Opt;
 
 
 /// Extends `nvim::Session` with optional `nvim_process` field.
@@ -287,6 +209,7 @@ impl <'a> NvimManager<'a> {
 }
 
 
+/// Represents usecase
 enum Page<'a> {
     Instance {
         name: &'a str,
