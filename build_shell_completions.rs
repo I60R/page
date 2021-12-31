cf536604458b760c7544c0edc95b40ca2bb97c14
160R <160R@protonmail.com>
2021-12-31 04:31:09 +0100

    migrated to clap

--- build_shell_completions.rs
+++ build_shell_completions.rs
@@ -1,4 +1,6 @@
-use structopt::clap::Shell;
+use clap::IntoApp;
+use clap_generate::generators::{Zsh, Bash, Fish};
+
 use std::{env, fs, error::Error, path::PathBuf};
 
 include!("src/pager/cli.rs");
@@ -8,9 +10,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap()).join("shell_completions");
     eprintln!("Shell completions would be generated in the following dir: {}", out_dir.display());
     fs::create_dir_all(&out_dir)?;
-    let mut app = Options::clap();
-    app.gen_completions("page", Shell::Zsh, &out_dir);
-    app.gen_completions("page", Shell::Bash, &out_dir);
-    app.gen_completions("page", Shell::Fish, &out_dir);
+    let mut app = Options::into_app();
+    clap_generate::generate_to(Zsh , &mut app, "page", &out_dir)?;
+    clap_generate::generate_to(Bash, &mut app, "page", &out_dir)?;
+    clap_generate::generate_to(Fish, &mut app, "page", &out_dir)?;
     Ok(())
 }
