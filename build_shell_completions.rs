82fcd4b5d21f23a27e985fd4c8b91cac21faa9f6
160R <160R@protonmail.com>
2022-12-15 19:57:09 +0100

    compile pager and picker depending on features

--- build_shell_completions.rs
+++ build_shell_completions.rs
@@ -4,11 +4,13 @@ use clap::CommandFactory;
 use std::{env, fs, error::Error, path::PathBuf};
 
 
+#[cfg(feature = "pager")]
 #[allow(dead_code)]
 mod pager {
     include!("src/pager/cli.rs");
 }
 
+#[cfg(feature = "picker")]
 #[allow(dead_code)]
 mod picker {
     include!("src/picker/cli.rs");
@@ -24,15 +26,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     fs::create_dir_all(&out_dir)?;
     eprintln!("Shell completions would be generated in: {}", out_dir.display());
 
-    let mut app = pager::Options::command();
-    clap_complete::generate_to(Zsh , &mut app, "page", &out_dir)?;
-    clap_complete::generate_to(Bash, &mut app, "page", &out_dir)?;
-    clap_complete::generate_to(Fish, &mut app, "page", &out_dir)?;
+    #[cfg(feature = "pager")]
+    {
+        let mut app = pager::Options::command();
+        clap_complete::generate_to(Zsh , &mut app, "page", &out_dir)?;
+        clap_complete::generate_to(Bash, &mut app, "page", &out_dir)?;
+        clap_complete::generate_to(Fish, &mut app, "page", &out_dir)?;
+    }
 
-    let mut app = picker::Options::command();
-    clap_complete::generate_to(Zsh , &mut app, "nv", &out_dir)?;
-    clap_complete::generate_to(Bash, &mut app, "nv", &out_dir)?;
-    clap_complete::generate_to(Fish, &mut app, "nv", &out_dir)?;
+    #[cfg(feature = "picker")]
+    {
+        let mut app = picker::Options::command();
+        clap_complete::generate_to(Zsh , &mut app, "nv", &out_dir)?;
+        clap_complete::generate_to(Bash, &mut app, "nv", &out_dir)?;
+        clap_complete::generate_to(Fish, &mut app, "nv", &out_dir)?;
+    }
 
     Ok(())
 }
