e7ad8b4298de414d3ffcfe671f5b98fb3a5dc469
160R <160R@protonmail.com>
2022-12-20 08:33:37 +0100

    refactor large functions in page/context

--- src/pager/context.rs
+++ src/pager/context.rs
@@ -19,48 +19,80 @@ pub mod gather_env {
     pub fn enter() -> EnvContext {
         let input_from_pipe = !atty::is(atty::Stream::Stdin);
 
-        let opt = {
-            let mut opt = crate::cli::get_options();
+        let opt = parse_and_alter_opts(input_from_pipe);
 
-            if opt.pagerize_hidden {
-                opt.pagerized();
+        let (term_height, prefetch_usage) = determine_prefetch_usage(
+            opt.output.noopen_lines,
+            opt.pagerize,
+            &opt.files,
+            input_from_pipe
+        );
+
+        let query_lines_count = determine_query_lines_count(
+            opt.output.query_lines,
+            term_height
+        );
+
+        EnvContext {
+            opt,
+            prefetch_usage,
+            query_lines_count,
+            input_from_pipe,
+        }
+    }
+
+
+    fn parse_and_alter_opts(input_from_pipe: bool) -> crate::cli::Options {
+        let mut opt = crate::cli::get_options();
+
+        // Remove some arguments from pagerized invocation
+        if opt.pagerize_hidden {
+            opt.pagerized();
+        }
+
+        // Fallback for neovim < 8.0 which don't uses $NVIM
+        if opt.address.is_none() {
+            if let Some(address) = std::env::var("NVIM_LISTEN_ADDRESS").ok() {
+                opt.address.replace(address);
             }
+        }
 
-            // Fallback for neovim < 8.0 which don't uses $NVIM
-            if opt.address.is_none() {
-                if let Some(address) = std::env::var("NVIM_LISTEN_ADDRESS").ok() {
-                    opt.address.replace(address);
-                }
-            }
+        // Treat empty -a value as if it wasn't provided
+        if opt.address.as_deref().map_or(false, str::is_empty) {
+            opt.address = None;
+        }
 
-            // Treat empty -a value as if it wasn't provided
-            if opt.address.as_deref().map_or(false, str::is_empty) {
-                opt.address = None;
-            }
+        // Override -O by -o, -p and -x flags and when page don't read from pipe
+        if opt.output_open ||
+            opt.pty_path_print ||
+            opt.instance_close.is_some() ||
+            (!input_from_pipe && opt.files.len() != 1)
+        {
+            opt.output.noopen_lines = None;
+        }
 
-            // Override -O by -o, -p and -x flags and when page don't read from pipe
-            if opt.output_open ||
-                opt.pty_path_print ||
-                opt.instance_close.is_some() ||
-                (!input_from_pipe && opt.files.len() != 1)
-            {
-                opt.output.noopen_lines = None;
-            }
+        opt
+    }
 
-            opt
-        };
-
+    fn determine_prefetch_usage(
+        noopen_lines: Option<Option<isize>>,
+        pagerize: Option<Option<usize>>,
+        files: &Vec<crate::cli::FileOption>,
+        input_from_pipe: bool
+    ) -> (usize, PrefetchLinesUsage) {
         use once_cell::unsync::Lazy;
+
         let term_dimensions = Lazy::new(|| {
             term_size::dimensions()
                 .expect("Cannot get terminal dimensions")
         });
+
         let (term_width, term_height) = (
             Lazy::new(|| term_dimensions.0),
             Lazy::new(|| term_dimensions.1),
         );
 
-        let mut prefetch_lines_count = match opt.output.noopen_lines {
+        let mut prefetch_lines_count = match noopen_lines {
             Some(Some(positive_number @ 0..)) => positive_number as usize,
             Some(Some(negative_number)) => term_height
                 .saturating_sub(negative_number.abs() as usize),
@@ -70,7 +102,7 @@ pub mod gather_env {
         };
 
         if prefetch_lines_count > 0 {
-            match opt.pagerize {
+            match pagerize {
                 Some(Some(n)) if prefetch_lines_count > n => {
                     prefetch_lines_count = n
                 },
@@ -81,33 +113,19 @@ pub mod gather_env {
             }
         }
 
-        let query_lines_count = match opt.output.query_lines {
-            Some(Some(positive_number @ 0..)) => positive_number as usize,
-            Some(Some(negative_number)) => term_height
-                .saturating_sub(negative_number.abs() as usize),
-            Some(None) => term_height
-                .saturating_sub(3),
-            None => 0,
-        };
-
         let mut prefetch_usage = PrefetchLinesUsage::Disabled;
-        if prefetch_lines_count != 0 &&
-            opt.files.is_empty() &&
-            input_from_pipe
-        {
+        if prefetch_lines_count != 0 && files.is_empty() && input_from_pipe {
             prefetch_usage = PrefetchLinesUsage::Enabled {
                 line_count: prefetch_lines_count,
                 term_width: *term_width,
                 source: PrefetchLinesSource::Stdin,
             };
-        } else if prefetch_lines_count != 0 &&
-            opt.files.len() == 1 &&
-            !input_from_pipe
-        {
-            if let crate::cli::FileOption::Path(f) = opt.files
+        } else if prefetch_lines_count != 0 && files.len() == 1 && !input_from_pipe {
+            let last_file = files
                 .last()
-                .unwrap()
-            {
+                .unwrap();
+
+            if let crate::cli::FileOption::Path(f) = last_file {
                 prefetch_usage = PrefetchLinesUsage::Enabled {
                     line_count: prefetch_lines_count,
                     term_width: *term_width,
@@ -116,12 +134,21 @@ pub mod gather_env {
             }
         }
 
+        (*term_height, prefetch_usage)
+    }
 
-        EnvContext {
-            opt,
-            prefetch_usage,
-            query_lines_count,
-            input_from_pipe,
+
+    fn determine_query_lines_count(
+        query_lines: Option<Option<isize>>,
+        term_height: usize
+    ) -> usize {
+        match query_lines {
+            Some(Some(positive_number @ 0..)) => positive_number as usize,
+            Some(Some(negative_number)) => term_height
+                .saturating_sub(negative_number.abs() as usize),
+            Some(None) => term_height
+                .saturating_sub(3),
+            None => 0,
         }
     }
 
@@ -196,29 +223,14 @@ pub mod check_usage {
 
         let prefetched_lines = PrefetchedLines(vec![]);
 
-        let tmp_dir = {
-            let d = std::env::temp_dir()
-                .join("neovim-page");
-            std::fs::create_dir_all(&d)
-                .expect("Cannot create temporary directory for page");
-            d
-        };
+        let tmp_dir = create_temp_directory();
 
-        let page_id = {
-            // This should provide enough entropy for current use case
-            let pid = std::process::id();
-            let time = std::time::UNIX_EPOCH.elapsed()
-                .unwrap()
-                .as_nanos();
-            format!("{pid}{time}")
-        };
+        let page_id = create_page_id();
 
-        let print_protection = {
-            !input_from_pipe &&
-            !opt.page_no_protect &&
-            std::env::var_os("PAGE_REDIRECTION_PROTECT")
-                .map_or(true, |v| v != "" && v != "0")
-        };
+        let print_protection = determine_if_should_print_protection(
+            input_from_pipe,
+            opt.page_no_protect,
+        );
 
         UsageContext {
             opt,
@@ -229,6 +241,32 @@ pub mod check_usage {
             prefetched_lines,
             query_lines_count,
         }
+    }
+
+    fn create_temp_directory() -> std::path::PathBuf {
+        let d = std::env::temp_dir()
+            .join("neovim-page");
+        std::fs::create_dir_all(&d)
+            .expect("Cannot create temporary directory for page");
+        d
+    }
+
+    fn create_page_id() -> String {
+        let pid = std::process::id();
+        let time = std::time::UNIX_EPOCH.elapsed()
+            .unwrap()
+            .as_nanos();
+        format!("{pid}{time}")
+    }
+
+
+    fn determine_if_should_print_protection(
+        input_from_pipe: bool,
+        page_no_protect: bool,
+    ) -> bool {
+        !input_from_pipe && !page_no_protect &&
+        std::env::var_os("PAGE_REDIRECTION_PROTECT")
+            .map_or(true, |protect| protect != "" && protect != "0")
     }
 
     pub struct PrefetchedLines(pub Vec<Vec<u8>>);
@@ -285,30 +323,19 @@ pub mod connect_neovim {
             ..
         } = cli_ctx;
 
-        let mut inst_usage = InstanceUsage::Disabled;
-        if let Some(name) = opt.instance.clone() {
-            inst_usage = InstanceUsage::Enabled {
-                name,
-                focused: true,
-                replace_content: true
-            }
-        } else if let Some(name) = opt.instance_append.clone() {
-            inst_usage = InstanceUsage::Enabled {
-                name,
-                focused: should_focus_on_existed_instance_buffer,
-                replace_content: false
-            }
-        }
+        let inst_usage = determine_instance_usage(
+            &opt.instance,
+            &opt.instance_append,
+            should_focus_on_existed_instance_buffer
+        );
 
-        let mut outp_buf_usage = OutputBufferUsage::Disabled;
-        if opt.is_output_split_implied() {
-            outp_buf_usage = OutputBufferUsage::CreateSplit
-        } else if input_from_pipe ||
-            opt.is_output_implied() ||
-            (opt.instance_close.is_none() && opt.files.is_empty())
-        {
-            outp_buf_usage = OutputBufferUsage::CreateSubstituting
-        }
+        let outp_buf_usage = determine_output_buffer_usage(
+            opt.is_output_split_implied(),
+            opt.is_output_implied(),
+            &opt.instance_close,
+            &opt.files,
+            input_from_pipe
+        );
 
         NeovimContext {
             opt,
@@ -320,6 +347,50 @@ pub mod connect_neovim {
             input_from_pipe,
             nvim_child_proc_spawned: false,
         }
+    }
+
+    fn determine_instance_usage(
+        instance: &Option<String>,
+        instance_append: &Option<String>,
+        should_focus_on_existed_instance_buffer: bool
+    ) -> InstanceUsage {
+        let mut inst_usage = InstanceUsage::Disabled;
+
+        if let Some(name) = instance.clone() {
+            inst_usage = InstanceUsage::Enabled {
+                name,
+                focused: true,
+                replace_content: true
+            }
+        } else if let Some(name) = instance_append.clone() {
+            inst_usage = InstanceUsage::Enabled {
+                name,
+                focused: should_focus_on_existed_instance_buffer,
+                replace_content: false
+            }
+        }
+
+        inst_usage
+    }
+
+    fn determine_output_buffer_usage(
+        is_output_split_implied: bool,
+        is_output_implied: bool,
+        instance_close: &Option<String>,
+        files: &Vec<crate::cli::FileOption>,
+        input_from_pipe: bool,
+    ) -> OutputBufferUsage {
+        let mut outp_buf_usage = OutputBufferUsage::Disabled;
+
+        if is_output_split_implied {
+            outp_buf_usage = OutputBufferUsage::CreateSplit
+        } else if input_from_pipe || is_output_implied ||
+            (instance_close.is_none() && files.is_empty())
+        {
+            outp_buf_usage = OutputBufferUsage::CreateSubstituting
+        }
+
+        outp_buf_usage
     }
 
 
@@ -422,23 +493,16 @@ pub mod output_buffer_available {
             ..
         } = nvim_ctx;
 
-        let mut restore_initial_buf_focus = RestoreInitialBufferFocus::Disabled;
-        if !nvim_child_proc_spawned {
-            if opt.back {
-                restore_initial_buf_focus = RestoreInitialBufferFocus::ViModeNormal
-            } else if opt.back_restore {
-                restore_initial_buf_focus = RestoreInitialBufferFocus::ViModeInsert
-            }
-        }
+        let restore_initial_buf_focus = determine_restore_initial_buffer_focus(
+            nvim_child_proc_spawned,
+            opt.back,
+            opt.back_restore
+        );
+
+        let pagerized_page_size = determine_pagerized_page_size(&opt.pagerize);
 
         let print_output_buf_pty = opt.pty_path_print ||
             (!nvim_child_proc_spawned && !input_from_pipe);
-
-        let pagerized_page_size = match opt.pagerize {
-            Some(Some(number)) => Some(number),
-            Some(None) => Some(100_000),
-            None => None,
-        };
 
         OutputContext {
             opt,
@@ -452,6 +516,35 @@ pub mod output_buffer_available {
             print_output_buf_pty,
             page_id,
             pagerized_page_size,
+        }
+    }
+
+    fn determine_restore_initial_buffer_focus(
+        nvim_child_proc_spawned: bool,
+        back: bool,
+        back_restore: bool,
+    ) -> RestoreInitialBufferFocus {
+        let mut restore_initial_buf_focus = RestoreInitialBufferFocus::Disabled;
+
+        if !nvim_child_proc_spawned {
+            if back {
+                restore_initial_buf_focus = RestoreInitialBufferFocus::ViModeNormal
+            } else if back_restore {
+                restore_initial_buf_focus = RestoreInitialBufferFocus::ViModeInsert
+            }
+        }
+
+        restore_initial_buf_focus
+    }
+
+
+    fn determine_pagerized_page_size(
+        pagerize: &Option<Option<usize>>
+    ) -> Option<usize> {
+        match pagerize {
+            Some(Some(number)) => Some(*number),
+            Some(None) => Some(100_000),
+            None => None,
         }
     }
 
